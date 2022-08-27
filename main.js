5b69dd807edb0808bcc56229ad1a18f324ef50e2
luorageoffrey <geoffrey.luora@student.moringaschool.com>
2022-08-27 12:19:46 +0300

    done with assigment

--- main.js
+++ main.js
@@ -4,6 +4,77 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let clickStatus = []
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+  let likeIcons = document.querySelectorAll('.like-glyph')
+
+  likeIcons.forEach((icon, index)=>{
+
+    clickStatus.push(false)
+
+    icon.setAttribute('id', `${index}`)
+    icon.addEventListener('click', (event)=>{
+
+      let serverCall = mimicServerCall()
+
+      serverCall.then(
+        (result)=>{
+
+          console.log("success")
+          console.log(result)
+          let icon = event.target
+          let index = event.target.getAttribute('id')
+
+          let isClicked = clickStatus[index]
+
+          console.log(icon)
+
+          console.log(index)
+
+          console.log(likeIcons)
+
+          if(isClicked){
+            icon.innerHTML = EMPTY_HEART
+            //icon.style = "background-color:white"
+            clickStatus[index] = false
+          }else{
+            icon.innerHTML = FULL_HEART
+            //icon.style = "background-color:red"
+            clickStatus[index] = true
+          }
+
+          console.log(likeIcons)
+
+          
+
+          console.log(icon)
+        },
+      
+        ).catch(
+          (error)=> handleError(error)
+        )
+
+      //console.log("clicked")
+    })
+  })
+})
+
+
+
+function handleError(error){
+  console.log(error, "from catch")
+  let modal = document.querySelector('#modal')
+
+  modal.removeAttribute('class', 'hidden')
+
+  setTimeout(function(){
+    modal.setAttribute('class', 'hidden')
+  },1000)
+
+}
+
 
 
 
@@ -23,3 +94,5 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
